fb9d8fb3d1b5cacbaee98ab8f1183a6d50320cd9
Denis <Denis.lkg@gmail.com>
2022-03-13 13:05:49 +0300

    react-quiz create

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Layout from './hoc/Layout/Layout';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout>
+
+        <div style={{ width: '400px', border: ' 1px solid #ccc' }}>
+          <h1>Layout</h1>
+        </div>
+      </Layout>
     </div>
   );
 }
