40dd856ce5382bfc4ebad5bea70c0068629ab2ce
DenisLo-master <66447463+DenisLo-master@users.noreply.github.com>
2022-03-17 14:34:53 +0300

    Dev (#4)
    * chapter 9 finish

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,10 +1,155 @@
 import React, { Component } from "react";
+import classes from './Auth.module.css'
+import Button from '../../components/UI/Button/Button'
+import Input from '../../components/UI/Input/Input'
+import is from 'is_js'
+
+
+// function validateEmail(email) {
+//     return String(email)
+//         .toLowerCase()
+//         .match(
+//             /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+//         );
+// };
 
 export default class Auth extends Component {
+
+    state = {
+        isFormValid: false,
+        formControls: {
+            email: {
+                value: '',
+                type: 'email',
+                label: 'Email',
+                errorMessage: 'Ввeдите корректный email',
+                valid: false,
+                touched: false,
+                validation: {
+                    required: true,
+                    email: true
+                }
+            },
+            password: {
+                value: '',
+                type: 'password',
+                label: 'Пароль',
+                errorMessage: 'Ввeдите корректный пароль',
+                valid: false,
+                touched: false,
+                validation: {
+                    required: true,
+                    minLength: 6
+                }
+            }
+        }
+    }
+
+
+    loginHandler() {
+
+    }
+
+    registrHandler() {
+
+    }
+
+    submitHandler = event => {
+        // чтобы не перезагружалась страница целиком при нажатии кнопки
+        event.preventDefault()
+    }
+
+    validateControl(value, validation) {
+        if (!validation) {
+            return true
+        }
+
+        let isValid = true
+
+        if (validation.required) {
+            isValid = value.trim() !== '' && isValid
+        }
+
+        if (validation.email) {
+            isValid = is.email(value) && isValid
+        }
+
+        if (validation.minLength) {
+            isValid = value.length >= validation.minLength && isValid
+        }
+
+        return isValid
+    }
+
+    onChangeHandler = (event, controlName) => {
+        // console.log(controlName, event.target.value)
+
+        const formControls = { ...this.state.formControls }
+        const control = { ...formControls[controlName] }
+
+        control.value = event.target.value
+        control.touched = true
+        control.valid = this.validateControl(control.value, control.validation)
+
+        formControls[controlName] = control
+
+        let isFormValid = true
+
+        Object.keys(formControls).forEach(name => {
+            isFormValid = formControls[name].valid && isFormValid
+        })
+
+
+        this.setState({
+            formControls, isFormValid
+        })
+
+    }
+
+    renderInputs() {
+        const inputs = Object.keys(this.state.formControls).map((controlName, index) => {
+            const control = this.state.formControls[controlName]
+            return (
+                <Input
+                    key={controlName + index}
+                    type={control.type}
+                    value={control.value}
+                    valid={control.valid}
+                    touched={control.touched}
+                    label={control.label}
+                    errorMessage={control.errorMessage}
+                    shouldValidate={!!control.validation}
+                    onChange={event => this.onChangeHandler(event, controlName)}
+                />
+            )
+        })
+        return inputs
+    }
+
+
+
     render() {
         return (
-            <div>
-                <h1>Auth</h1>
+            <div className={classes.Auth}>
+                <div>
+                    <h1>Авторизация</h1>
+                    <form className={classes.AuthForm} onSubmit={this.submitHandler}>
+                        {this.renderInputs()}
+                        <Button
+                            type="success"
+                            onClick={this.loginHandler}
+                            disabled={!this.state.isFormValid}
+                        >
+                            Войти
+                        </Button>
+                        <Button
+                            type="primary"
+                            onClick={this.registrHandler}
+                        >
+                            Зарегистроваться
+                        </Button>
+                    </form>
+                </div>
             </div>
         )
     }
--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -1,10 +1,169 @@
 import React, { Component } from "react";
+import classes from './QuizCreator.module.css'
+import Input from '../../components/UI/Input/Input'
+import Button from '../../components/UI/Button/Button'
+import { createControl, validate, validateForm } from '../../from/formFramework'
+import Auxiliary from '../../hoc/Auxiliary/Auxiliary'
+import Select from "../../components/UI/Select/Select";
+
+function createOptionControl(number) {
+    return createControl({
+        label: `Вариант ${number}`,
+        errorMessage: 'Значение не может быть пустым',
+        id: number,
+    }, { required: true })
+}
+
+function createFormControls(questionId) {
+    return {
+        question: createControl({
+            label: `Введите вопрос № ${questionId}`,
+            errorMessage: 'Вопрос не может быть пустым',
+        }, { required: true }),
+        option1: createOptionControl(1),
+        option2: createOptionControl(2),
+        option3: createOptionControl(3),
+        option4: createOptionControl(4),
+    }
+}
 
 export default class QuizCreator extends Component {
+
+    state = {
+        quiz: [],
+        isFormValid: false,
+        rightAnswerId: 1,
+        formControls: createFormControls(1)
+    }
+
+    submitHandler = event => {
+        // чтобы не перезагружалась страница целиком при нажатии кнопки
+        event.preventDefault()
+    }
+
+    addQuestionHandler = event => {
+        // чтобы не перезагружалась страница целиком при нажатии кнопки
+        event.preventDefault()
+
+        // concat( ) возвращает копию массива, защита от мутаций первого при изменении
+        const quiz = this.state.quiz.concat()
+        const index = quiz.length + 1
+        const { question, option1, option2, option3, option4 } = this.state.formControls
+
+        const questionItem = {
+            question: question.value,
+            id: index,
+            rightAnswerId: this.state.rightAnswerId,
+            answers: [
+                { text: option1.value, id: option1.id },
+                { text: option2.value, id: option2.id },
+                { text: option3.value, id: option3.id },
+                { text: option4.value, id: option4.id },
+            ]
+        }
+        quiz.push(questionItem)
+
+        this.setState({
+            quiz,
+            isFormValid: false,
+            rightAnswerId: 1,
+            formControls: createFormControls(questionItem.id + 1)
+        })
+    }
+
+    createQuizHandler = event => {
+        event.preventDefault()
+
+        console.log(this.state.quiz)
+        //TODO: Server
+    }
+
+    ChangeHandler = (value, controlName) => {
+        const formControls = { ...this.state.formControls }
+        const control = { ...formControls[controlName] }
+
+        control.touched = true
+        control.value = value
+        control.valid = validate(control.value, control.validation)
+
+        formControls[controlName] = control
+
+        this.setState({
+            formControls,
+            isFormValid: validateForm(formControls)
+        })
+
+    }
+
+    renderControls() {
+        return Object.keys(this.state.formControls).map((controlName, index) => {
+            const control = this.state.formControls[controlName]
+            return (
+                //оборачиваем в hoc свой компонент "Auxiliary" можно React.Fragment, 
+                // т.к. return не может вернуть больше 1 объекта
+                <Auxiliary key={controlName + index}>
+                    <Input
+                        type={control.type}
+                        value={control.value}
+                        valid={control.valid}
+                        touched={control.touched}
+                        label={control.label}
+                        errorMessage={control.errorMessage}
+                        shouldValidate={!!control.validation}
+                        onChange={event => this.ChangeHandler(event.target.value, controlName)}
+                    />
+                    {index === 0 ? <hr /> : null}
+                </Auxiliary>
+            )
+        })
+    }
+
+    selectChangeHandler = event => {
+        this.setState({
+            rightAnswer: +event.target.value
+        })
+    }
+
     render() {
+
+        const select = <Select
+            label="Выберите правильный ответ"
+            value={this.state.rightAnswer}
+            onChange={this.selectChangeHandler}
+            options={[
+                { text: 1, value: 1 },
+                { text: 2, value: 2 },
+                { text: 3, value: 3 },
+                { text: 4, value: 4 },
+            ]}
+        />
+
+
+
         return (
-            <div>
-                <h1>QuizCreator</h1>
+            <div className={classes.QuizCreator}>
+                <div>
+                    <h1>QuizCreator</h1>
+                    <form onSubmit={this.submitHandler}>
+                        {this.renderControls()}
+                        {select}
+                        <Button
+                            type="primary"
+                            onClick={this.addQuestionHandler}
+                            disabled={!this.state.isFormValid}
+                        >
+                            Добавить вопрос
+                        </Button>
+                        <Button
+                            type="success"
+                            onClick={this.createQuizHandler}
+                            disabled={this.state.quiz.length === 0}
+                        >
+                            Создать тест
+                        </Button>
+                    </form>
+                </div>
+
             </div>
         )
     }
