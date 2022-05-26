fe9e18aeb78f07250f22b2bc26578a02d9c32931
Denis <Denis.lkg@gmail.com>
2022-05-26 16:03:55 +0300

    inp

--- src/index.js
+++ src/index.js
@@ -1,30 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { register } from 'register-service-worker'
-import { BrowserRouter } from 'react-router-dom'
-import { createStore, compose, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux'
-import rootReducer from './store/reducers/rootReducer';
-import thunk from 'redux-thunk'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { register } from "register-service-worker";
+import { BrowserRouter } from "react-router-dom";
+import { createStore, compose, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from "./store/reducers/rootReducer";
+import thunk from "redux-thunk";
 
 const composeEnhancers =
-  typeof window === 'object' &&
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-    }) : compose;
-
-
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
 
 const store = createStore(
   rootReducer,
-  composeEnhancers(
-    applyMiddleware(thunk)
-  )
-)
-
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 const app = (
   <Provider store={store}>
@@ -32,13 +26,9 @@ const app = (
       <App />
     </BrowserRouter>
   </Provider>
-)
-
-
-ReactDOM.render(
-  app,
-  document.getElementById('root')
 );
+
+ReactDOM.render(app, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
